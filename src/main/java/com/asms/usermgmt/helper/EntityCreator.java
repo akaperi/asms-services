591ed7ad7196e19adfae40325668db423c5e9e30
Gayithri Hegde <Gayithri Hegde@192.168.2.101>
2017-08-04 10:54:18 +0530

    committing entity creator

---
+++ src/main/java/com/asms/usermgmt/helper/EntityCreator.java
@@ -0,0 +1,49 @@
+package com.asms.usermgmt.helper;
+
+import org.springframework.stereotype.Component;
+
+import com.asms.usermgmt.entity.Student;
+import com.asms.usermgmt.entity.User;
+import com.asms.usermgmt.request.StudentDetails;
+
+/*
+ * EntityCreator.java does creation of entity objects
+ * from ui object
+ * 
+ */
+
+@Component
+public class EntityCreator {
+	
+	/*
+	 * Method: createStudent -> maps ui values to entity
+	 * input : StudentDetails
+	 * return : Student
+	 * 
+	 */
+	
+	public Student createStudent(StudentDetails details, User user) {
+		Student student = new Student();
+		student.setAdmissionDate(details.getAdmissionDate());
+		student.setAdmissionNo(details.getAdmissionNo());
+		
+		student.setSchoolId("SSC123");
+		student.setStudentAgeInYears(details.getStudentAgeInYears());
+		student.setStudentBirthplace(details.getStudentBirthplace());
+		student.setStudentCasteCategory(details.getStudentCasteCategory());
+		student.setStudentClass(details.getStudentClass());
+		student.setStudentCreatedByWadmin(user.getUserId());
+		student.setStudentDob(details.getStudentDob());
+		student.setStudentFirstName(details.getStudentFirstName());
+		student.setStudentGender(details.getStudentGender());
+		student.setStudentIdentificationMarks(details.getStudentIdentificationMarks());
+		student.setStudentLastName(details.getStudentLastName());
+		student.setStudentMiddleName(details.getStudentMiddleName());
+		student.setStudentMotherTongue(details.getStudentMotherTongue());
+		student.setStudentNationality(details.getStudentNationality());
+		student.setStudentPhoto(details.getStudentPhoto());
+		student.setUserObject(user);
+		return student;
+	}
+
+}
