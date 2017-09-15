02a61742a60c91bc83a4f95de9abbc82a6bcdc65
devendra.sc <devendra.sc@DEVENDERSC.corp.esika.in>
2017-09-15 11:41:18 +0530

    test

--- src/main/java/com/asms/common/helper/AsmsHelper.java
+++ src/main/java/com/asms/common/helper/AsmsHelper.java
@@ -8,6 +8,7 @@ public class AsmsHelper {
 	public static final ResourceBundle getMessageFromBundle(){
 		Locale currentLocale = new Locale("en", "US");
 		
+		
 		ResourceBundle messages = ResourceBundle.getBundle("spring.MessageBundles.ErrorMessages", currentLocale);
 		return messages;
 		
